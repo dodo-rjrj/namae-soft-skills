0da7b7d94480531756e4f9b074d2eefa11fdb137
anass <anassaitlahcen1975@gmail.com>
2025-05-16 23:22:00 +0100

    Ajouter graphe nombres d evaluations et filtre par semaine et mois

--- backend/routes/evaluationRoutes.js
+++ backend/routes/evaluationRoutes.js
@@ -6,7 +6,10 @@ router.get('/total', evaluationController.getTotalEvaluations);
 router.get('/moyenne', evaluationController.getMoyenneGlobale);
 router.get('/moyenne-par-competence', evaluationController.getMoyenneParCompetence);
 router.get('/par-jour', evaluationController.getEvaluationsParJour);
+router.get('/par-mois', evaluationController.getEvaluationsParMois);
+router.get('/stats', evaluationController.getStatsFiltrées);
 
 
 module.exports = router;
+
 
